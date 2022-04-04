a6bde5cb41025e48e445a1335c8bab4fffb11447
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-04-04 15:42:10 +0300

    create query builder

--- index.php
+++ index.php
@@ -1,67 +1,109 @@
 <?php
 
-class QueryBuilder
-{
-    private $fields = [];
-    private $conditions = [];
-    private $from = [];
-    public function __construct()
-    {
-        $this->reset();
-    }
 
-    public function __toString(): string
-    {
-        $where = $this->conditions === [] ? '' : ' WHERE ' . implode(' AND ', $this->conditions);
-        return 'SELECT ' . implode(', ', $this->fields)
-            . ' FROM ' . implode(', ', $this->from)
-            . $where;
-    }
-    public function select(string $select): self
-    {
-        $this->fields = $select;
-        return $this;
-    }
+require_once "db.php";
 
-    public function where(string $where): self
-    {
-        foreach ($where as $arg) {
-            $this->conditions[] = $arg;
-        }
-        return $this;
-    }
-    public function orderBy($sort, $order = null);
-    public function innerJoin($join, $conditionType = null, $condition = null, $indexBy = null);
-    public function groupBy($groupBy);
-    public function having($having);
+$user = new Database('super-man');
 
-   
+$result = $user
+  ->select('erp2, heros');
+
+
+print_r($result->where());
+
+exit;
+
+$user->setTable('school', [
+  'name' => 'VARCHAR(255) NOT NULL',
+  'age' => 'INT(12) DEFAULT 5',
+]);
+
+
+try {
+  $table_header_items = $user->select('erp2', [
+    'id', 'name', 'age', 'major'
+  ])->where()[0];
+} catch (\Throwable $th) {
+  $table_header_items = [];
 }
-$query = (new QueryBuilder())
-->select('email', 'first_name', 'last_name')
-->from('user');
 
-$Statement = $pdo->prepare($query);
-$Statement->execute();
+// $user->insert('erp2', [
+//   "name" => "new Naif",
+//   "age" => 20,
+//   "major" => "it"
+// ]);
 
-$users = $Statement->fetchAll(PDO::FETCH_ASSOC);
-$query = (new QueryBuilder())
-->select('u.email', 'u.first_name', 'u.last_name', 'u.active')
-->from('user', 'u')
-->where('u.email = :email', 'u.active = :bool');
-// ->innerJoin('u', 'phonenumbers', 'p', 'u.id = p.user_id');
-// ->groupBy('DATE(last_login)'),
-// ->having('users > 10');
-// ->orderBy('username', 'ASC')
-// ->addOrderBy('last_login', 'ASC NULLS FIRST');
+// $user->update('heros', 2, [
+//   "name" => "new Naif",
+//   "age" => 19
+// ]);
 
-
-$Statement = $pdo->prepare($query);
-$Statement->execute([
-    'email' => 'roor3hakimi@gmail.com', 
-    'bool' => 1
-    ]
-);
-
-$user = $Statement->fetch(PDO::FETCH_ASSOC);
+// $user->delete('heros', 1);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Super team</title>
+
+  <link rel="stylesheet" href="css/output.css">
+</head>
+
+<body class="dark:bg-gray-500 p-8">
+  <div class="flex flex-col">
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
+        <div class="overflow-hidden shadow-md sm:rounded-lg">
+          <table class="min-w-full">
+            <thead class="bg-gray-50 dark:bg-gray-700">
+              <tr>
+                <!-- table head names -->
+                <?php foreach ($table_header_items as $key => $value) {
+                  echo "
+                    <th scope='col' class='py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400'> {$key} </th>
+                    ";
+                }
+                ?>
+                <td></td>
+              </tr>
+            </thead>
+
+            <tbody>
+              <?php foreach ($result as $item) {  ?>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <?php echo $item['id']; ?>
+                  </td>
+                  <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                    <?php echo $item['name']; ?>
+                  </td>
+                  <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                    <?php echo $item['age']; ?>
+                  </td>
+                  <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                    <?php echo $item['major']; ?>
+                  </td>
+                  <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                    <form action="delete.php" method="post">
+                      <input type="hidden" name="id" value="<?php echo $item['id']; ?>">
+                      <button name="delete_item" type="submit" class="text-red-600 dark:text-red-500 hover:underline">delete</button>
+                    </form>
+                  </td>
+                </tr>
+
+              <?php  }  ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</body>
+
+</html>
